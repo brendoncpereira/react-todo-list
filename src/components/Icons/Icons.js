bb27999f73304373363fd2614bcdefce3d5d2964
brendoncpereira <brendoncastrop@gmail.com>
2023-03-13 23:31:50 -0300

    fix: deleteTask minor fix + dark mode elements styles 100%

--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export const DarkModeIcon = () => {
+export const DarkModeIcon = ({color}) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
             fill="none"
-            stroke="currentColor"
+            stroke={color ? color : "#F8F8FF"}
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
@@ -19,14 +19,14 @@ export const DarkModeIcon = () => {
     )
 }
 
-export const LightModeIcon = () => {
+export const LightModeIcon = ({color}) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
             fill="none"
-            stroke="currentColor"
+            stroke={color ? color : "#1D263B"}
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
@@ -46,14 +46,14 @@ export const LightModeIcon = () => {
     )
 }
 
-export const AddTaskIcon = () => {
+export const AddTaskIcon = ({color}) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
             fill="none"
-            stroke="#F8F8FF"
+            stroke={color ? color : "#F8F8FF"}
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
@@ -66,14 +66,14 @@ export const AddTaskIcon = () => {
     )
 }
 
-export const DeleteTaskIcon = () => {
+export const DeleteTaskIcon = ({color}) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
             fill="none"
-            stroke="#F8F8FF"
+            stroke={color ? color : "#F8F8FF"}
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
@@ -82,6 +82,28 @@ export const DeleteTaskIcon = () => {
         >
             <path d="M3 6L5 6 21 6"></path>
             <path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"></path>
+        </svg>
+    )
+}
+
+export const EmptyStateIcon = ({color}) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="80"
+            height="80"
+            version="1"
+            viewBox="0 0 512 512"
+            fill={color ? color : "#1D263B"}
+        >
+            <path
+                d="M1880 5104c-168-44-305-172-354-328l-18-56h-316c-192 0-332-4-355-11-21-6-56-28-77-49l-39-39-248-3-248-3-57-28c-64-32-111-80-142-146l-21-46V225l21-46c31-66 78-114 142-146l57-28h3550l57 28c64 32 111 80 142 146l21 46v4170l-21 46c-31 66-78 114-142 146l-57 28-248 3-248 3-39 39c-21 21-56 43-77 49-23 7-163 11-355 11h-316l-21 61c-82 243-347 388-591 323zm254-218c90-44 150-129 162-233 3-26 10-61 16-76 21-55 34-57 428-57h360v-400H900v400h360c257 0 367 3 385 12 31 14 55 56 55 96 0 46 28 126 59 169 84 116 243 154 375 89zM700 4320v-100H590c-63 0-120-5-135-12-58-26-55 93-55-1897 0-1398 3-1836 12-1856 25-55 21-55 613-55s588 0 613 55c15 33 15 57 0 90-25 55-26 55-553 55H600v3420h60c57 0 62-2 99-39 22-22 57-44 78-50 53-15 2273-15 2326 0 21 6 56 28 78 50 37 37 42 39 99 39h60V600h-485c-527 0-528 0-553-55-15-33-15-57 0-90 25-55 21-55 613-55s588 0 613 55c17 38 17 3672 0 3710-20 44-55 55-178 55h-110v200h210c229 0 255-5 278-55 17-38 17-4072 0-4110-26-58 84-55-1788-55s-1762-3-1788 55c-19 40-17 4080 2 4114 26 46 47 50 274 51h212v-100z"
+                transform="matrix(.1 0 0 -.1 0 512)"
+            ></path>
+            <path
+                d="M1951 4706c-87-48-50-186 49-186 51 0 100 49 100 99 0 75-83 124-149 87zM1124 2740c-35-14-64-59-64-98 0-30 8-44 49-83 70-68 141-93 261-94 83 0 102 3 153 27 118 55 169 116 151 180-21 73-105 93-172 39-22-17-51-35-66-41-63-24-149-4-216 49-31 25-68 33-96 21zM2385 2736c-39-17-55-37-61-78-16-98 168-207 330-195 98 6 178 39 237 96 41 40 49 53 49 84 0 84-94 130-157 79-69-56-155-76-219-52-15 6-43 23-63 39-46 36-78 43-116 27zM1789 2041c-40-40-40-89 0-133l29-33h364l29 33c40 44 40 93 0 133l-29 29h-364l-29-29zM1951 586c-87-48-50-186 49-186 51 0 100 49 100 99 0 75-83 124-149 87zM4571 5106c-47-26-63-84-37-135 22-41 55-51 176-51h110l-151-202c-148-197-151-203-146-242 4-27 15-49 32-63 26-22 32-23 265-23s239 1 265 23c17 14 28 36 32 63 5 36 1 46-25 75l-30 34-116 3c-64 2-116 6-116 8 0 3 60 84 133 182 74 97 139 187 145 199 25 45 6 105-39 129-39 20-462 20-498 0zM4274 4081c-40-17-66-62-61-105 8-61 35-80 121-86l74-5-100-135c-96-128-100-136-95-175 4-26 15-48 32-62 25-22 33-23 215-23s190 1 215 23c17 14 28 36 32 63 5 36 1 46-25 75-27 30-36 33-96 37-36 2-66 6-66 9 0 2 41 58 91 124 109 145 119 177 71 230l-30 34-179 2c-98 1-188-2-199-6z"
+                transform="matrix(.1 0 0 -.1 0 512)"
+            ></path>
         </svg>
     )
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from './styles.module.css';
-import { AddTaskIcon } from '../Icons/Icons';
-import { DeleteTaskIcon } from "../Icons/Icons";
-import { LightModeIcon } from "../Icons/Icons";
-import { DarkModeIcon } from "../Icons/Icons";
+import { AddTaskIcon, DeleteTaskIcon, LightModeIcon, DarkModeIcon, EmptyStateIcon } from '../Icons/Icons';
+
 
 const TodoList = () => {
 
@@ -11,7 +9,13 @@ const TodoList = () => {
   const [newTasks, setNewTasks] = useState([])
   const [darkMode, setDarkMode] = useState(false)
 
+  const colorSettings1 = darkMode ? "#F8F8FF" : "#1D263B"
+  const colorSettings2 = darkMode ? "#1D263B" : "#F8F8FF"
+
+  const inputRef = useRef(null);
+
   const handleChangeTask = (e) => {
+
     setCurrentTask(e.target.value)
   }
 
@@ -30,9 +34,11 @@ const TodoList = () => {
   const handleDeleteTask = (index) => {
     const updatedTasks = [...newTasks]
 
-    updatedTasks?.splice(index, 1)
-
-    setNewTasks(updatedTasks)
+    if (updatedTasks.length > 0) {
+      updatedTasks?.splice(index, 1)
+      setNewTasks(updatedTasks)
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    }
   }
 
   const handleToggleMode = () => {
@@ -46,37 +52,39 @@ const TodoList = () => {
   useEffect(() => {
     const tasks = localStorage.getItem("tasks")
     if (tasks) {
-      setNewTasks(JSON.parse(tasks))
+      setNewTasks(JSON.parse(tasks));
     }
-
   }, [])
 
   useEffect(() => {
 
     if (newTasks.length > 0) {
-      localStorage.setItem("tasks", JSON.stringify(newTasks))
+      localStorage.setItem("tasks", JSON.stringify(newTasks));
     }
   }, [newTasks])
 
   useEffect(() => {
     const savedDarkMode = localStorage.getItem("darkMode")
     if (savedDarkMode) {
-      setDarkMode(JSON.parse(savedDarkMode))
+      setDarkMode(JSON.parse(savedDarkMode));
     }
+  }, [])
 
-    console.log(savedDarkMode)
-
-  }, [darkMode])
+  useEffect(() => {
+    if (newTasks) {
+      inputRef.current.focus()
+    }
+  }, [newTasks])
 
 
   return (
     <div className={`${styles.container} ${darkMode ? styles.containerDarkMode : ""}`}>
 
       <div className={`${styles.wrapper} ${darkMode ? styles.wrapperDarkMode : ""}`}>
-        <h1 className={styles.title}>Lista de Tarefas</h1>
+        <h1 className={`${styles.title} ${darkMode ? styles.titleDarkMode : ""}`}>Lista de Tarefas</h1>
 
         <div className={styles.labelContainer}>
-          <h2 className={styles.label}>Adicione uma nova tarefa</h2>
+          <h2 className={`${styles.label} ${darkMode ? styles.labelDarkMode : ""}`}>Adicione uma nova tarefa</h2>
         </div>
 
         <div className={styles.inputWrapper}>
@@ -86,17 +94,23 @@ const TodoList = () => {
               onChange={handleChangeTask}
               value={currentTask}
               placeholder="Ex.: Lavar a louça"
-              className={styles.taskInput}
+              ref={inputRef}
+              className={`${styles.taskInput} ${darkMode ? styles.taskInputDarkMode : ""}`}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && currentTask.trim() !== "") {
+                  handleAddTask()
+                }
+              }}
             />
           </div>
 
           <div className={styles.addTaskButtonContainer}>
             <button
               onClick={handleAddTask}
-              className={`${styles.addTaskButton} ${darkMode ? "" : styles.addTaskButtonDarkMode}`}
-              style={currentTask === "" ? { cursor: "not-allowed", opacity: 0.9, } : null}
+              className={`${styles.addTaskButton} ${darkMode ? styles.addTaskButtonDarkMode : ""}`}
+              style={currentTask === "" ? { cursor: "not-allowed" } : null}
             >
-              <AddTaskIcon />
+              <AddTaskIcon color={colorSettings2} />
             </button>
           </div>
         </div>
@@ -111,22 +125,23 @@ const TodoList = () => {
                       <div className={styles.liTaskWrapper}>
                         <li
                           key={task}
-                          className={styles.task}
+                          className={`${styles.task} ${darkMode ? styles.taskDarkMode : ""}`}
                         >
                           {task}
                         </li>
                         <button
                           onClick={() => { handleDeleteTask(index) }}
-                          className={`${styles.deleteTaskButton} ${darkMode ? "" : styles.deleteTaskButtonDarkMode}`}
+                          className={`${styles.deleteTaskButton} ${darkMode ? styles.deleteTaskButtonDarkMode : ""}`}
                         >
-                          <DeleteTaskIcon />
+                          <DeleteTaskIcon color={colorSettings2} />
                         </button>
                       </div>
                     </>
                   )
                 }) : (
-                  <div>
-                    <span>Nada por aqui, tente adicionar uma nova tarefa :)</span>
+                  <div className={styles.emptyStateContainer}>
+                    <EmptyStateIcon color={colorSettings1} />
+                    <p className={`${styles.emptyStateText} ${darkMode ? styles.emptyStateTextDarkMode : ""}`}>Nada novo por aqui...<br /> <span>Tente adicionar algumas tarefas.</span></p>
                   </div>
                 )
             }
@@ -140,9 +155,9 @@ const TodoList = () => {
             title={darkMode ? "Light mode" : "Dark mode"}
           >
             {darkMode ?
-              <LightModeIcon />
+              <LightModeIcon color={"#1D263B"} />
               :
-              <DarkModeIcon />
+              <DarkModeIcon color={"#F8F8FF"} />
             }
           </button>
         </div>
